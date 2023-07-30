d5be0f16a657cdfe467efd9b99deefbb15bf5b6b
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2023-07-30 22:50:55 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,28 @@
+/* var abc = document.getElementsByClassName("kumar");
+console.log(abc);//
+abc[0].style.padding= '0.75rem 1.25rem';
+abc[0].style.display= 'box';
+abc[0].style.margin = '-1px';
+abc[0].style.backgroundColor = 'white'; 
+abc[0].style.borderRadius = '4px 3px 3px 3px';
+abc[0].style.border = "1px solid rgba(0,0,0,.125 ";
+abc[0].style.display = "box"
+abc[0].style.marginBottom = '-1px'
+abc[0].style.boxsizing = "inherit"
+abc[0].style.listStyle = "none" */
+
+
+var li = document.getElementsByTagName("li");
+
+li[4].style.listStyle = "none"
+li[4].style.padding= '0.75rem 1.25rem';
+li[4].style.display= 'box';
+li[4].style.margin = '-1px';
+li[4].style.backgroundColor = 'white'; 
+li[4].style.borderRadius = '4px 3px 3px 3px';
+li[4].style.border = "1px solid rgba(0,0,0,.125 ";
+li[4].style.display = "box"
+li[4].style.marginBottom = '-1px'
+li[4].style.boxsizing = "inherit"
+li[4].style.listStyle = "none"
+li[4].style.textAlign = 'left'
