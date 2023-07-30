9d10b6e49d8bec95b37eab52bef84d65df08f9b8
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2023-07-31 00:33:14 +0530

    Update j.js

--- j.js
+++ j.js
@@ -3,10 +3,10 @@ console.log(title) */
 
 var odd = document.querySelectorAll('l1:nth-child(odd)');
 console.log(odd);
-/* var odd = document.querySelectorAll('l1:nth-child(even)');
+ var odd = document.querySelectorAll('l1:nth-child(even)');
 for(let i=0; i < odd.length; i++)
 {
     odd[i].style.backgroundColor = '#f4f4f4';
     even[i].style.backgroundColor = '#ccc';
 
-} */
+}
