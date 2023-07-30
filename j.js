4e73cadda8e94c9bf12d4ebc4a55580ad8d0a81f
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2023-07-31 00:32:31 +0530

    Add files via upload

--- j.js
+++ j.js
@@ -1,28 +1,12 @@
-/* var abc = document.getElementsByClassName("kumar");
-console.log(abc);//
-abc[0].style.padding= '0.75rem 1.25rem';
-abc[0].style.display= 'box';
-abc[0].style.margin = '-1px';
-abc[0].style.backgroundColor = 'white'; 
-abc[0].style.borderRadius = '4px 3px 3px 3px';
-abc[0].style.border = "1px solid rgba(0,0,0,.125 ";
-abc[0].style.display = "box"
-abc[0].style.marginBottom = '-1px'
-abc[0].style.boxsizing = "inherit"
-abc[0].style.listStyle = "none" */
+/* var title = document.querySelectorAll('title');
+console.log(title) */
 
+var odd = document.querySelectorAll('l1:nth-child(odd)');
+console.log(odd);
+/* var odd = document.querySelectorAll('l1:nth-child(even)');
+for(let i=0; i < odd.length; i++)
+{
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
 
-var li = document.getElementsByTagName("li");
-
-li[4].style.listStyle = "none"
-li[4].style.padding= '0.75rem 1.25rem';
-li[4].style.display= 'box';
-li[4].style.margin = '-1px';
-li[4].style.backgroundColor = 'white'; 
-li[4].style.borderRadius = '4px 3px 3px 3px';
-li[4].style.border = "1px solid rgba(0,0,0,.125 ";
-li[4].style.display = "box"
-li[4].style.marginBottom = '-1px'
-li[4].style.boxsizing = "inherit"
-li[4].style.listStyle = "none"
-li[4].style.textAlign = 'left'
+} */
